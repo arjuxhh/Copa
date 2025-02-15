5d8f62fbd857db986f985821693d42b22c5c6ec6
arjuxhh <arjuneeh@gmail.com>
2025-02-15 11:27:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ DATABASE_NAME = environ.get('DATABASE_NAME', "")  # Add the actual database name
 COLLECTION_NAME = "Telegram_files"
 USE_CAPTION_FILTER = True
 IMDB = "some_value"
-IMDB_TEMPLATE = "some_value"
+IMDB_TEMPLATE = environ.get("IMDB_TEMPLATE", "<code>{query}</code>\n\n<b>〓〓〓 <a href={url}>{title}</a> 〓〓〓\n\n⭐️ ɪᴍᴅʙ  {rating} | ⏰ ʀᴜɴ {runtime} ᴍɪɴ |\n📆 ʀᴇʟᴇᴀsᴇ ᴅᴀᴛᴇ : {release_date}</b>\n\n● <code>{genres}</code>\n● <code>languages</code>\n\n📖 <b>ꜱᴛᴏʀʏ</b> : {plot}\n\n<b><i>★ ᴘᴏᴡᴇʀᴇᴅ ʙʏ</i></b> <a href='https://t.me/moviehub_support'><b><i>ᴍᴏᴠɪᴇʜᴜʙ</i></b></a>")
 MELCOW_NEW_USERS = True
 P_TTI_SHOW_OFF = False
 SINGLE_BUTTON = True
