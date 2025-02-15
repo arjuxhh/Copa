94cd6e8b8cd97bb3f8df748ad867a8fb3a44c296
arjuxhh <arjuneeh@gmail.com>
2025-02-15 11:18:00 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+# config.py
+DATABASE_URI = "your_database_uri"
+DATABASE_NAME = "your_database_name"
+COLLECTION_NAME = "your_collection_name"
+USE_CAPTION_FILTER = True
+IMDB = "some_value"
+IMDB_TEMPLATE = "some_value"
+MELCOW_NEW_USERS = True
+P_TTI_SHOW_OFF = False
+SINGLE_BUTTON = True
+SPELL_CHECK_REPLY = True
+PROTECT_CONTENT = False
