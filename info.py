d25cf686e9f478b572b61b6165844ae80a95436a
arjuxhh <arjuneeh@gmail.com>
2025-02-15 11:00:38 +0530

    Update info.py

--- info.py
+++ info.py
@@ -32,7 +32,7 @@ AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
 
-DATABASE_NAME = "mydatabase"
+DATABASE_NAME = environ.get('DATABASE_NAME', "")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 from motor.motor_asyncio import AsyncIOMotorClient
 
