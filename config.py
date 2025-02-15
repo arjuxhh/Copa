6ee936bd42390f3c46356d79f8e821d51866cc19
arjuxhh <arjuneeh@gmail.com>
2025-02-15 11:29:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,8 +7,8 @@ COLLECTION_NAME = "Telegram_files"
 USE_CAPTION_FILTER = True
 IMDB = "some_value"
 IMDB_TEMPLATE = environ.get("IMDB_TEMPLATE", "<code>{query}</code>\n\n<b>〓〓〓 <a href={url}>{title}</a> 〓〓〓\n\n⭐️ ɪᴍᴅʙ  {rating} | ⏰ ʀᴜɴ {runtime} ᴍɪɴ |\n📆 ʀᴇʟᴇᴀsᴇ ᴅᴀᴛᴇ : {release_date}</b>\n\n● <code>{genres}</code>\n● <code>languages</code>\n\n📖 <b>ꜱᴛᴏʀʏ</b> : {plot}\n\n<b><i>★ ᴘᴏᴡᴇʀᴇᴅ ʙʏ</i></b> <a href='https://t.me/moviehub_support'><b><i>ᴍᴏᴠɪᴇʜᴜʙ</i></b></a>")
-MELCOW_NEW_USERS = True
+MELCOW_NEW_USERS = is_enabled((environ.get('MELCOW_NEW_USERS', "False")), False)
+PROTECT_CONTENT = is_enabled((environ.get('PROTECT_CONTENT', "False")), False)
 P_TTI_SHOW_OFF = False
 SINGLE_BUTTON = True
 SPELL_CHECK_REPLY = True
-PROTECT_CONTENT = False
